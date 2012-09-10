39a0ccf15f4d1522b58c7ec5d93c3fa624548d6e
Gabriel Nistor <gabriel.nistor@sourcefabric.org>
2012-09-10 11:13:26 +0300

    NR-148 : Meta refactor
    Merged with master and fixed the authentichation component.

--- components/ally-authentication-http/__setup__/ally_authentication_http/resources.py
+++ components/ally-authentication-http/__setup__/ally_authentication_http/resources.py
@@ -1,7 +1,7 @@
 '''
 Created on Jun 1, 2012
 
-@package: ally core http
+@package: ally authentication http
 @copyright: 2012 Sourcefabric o.p.s.
 @license: http://www.gnu.org/licenses/gpl-3.0.txt
 @author: Gabriel Nistor
@@ -9,7 +9,8 @@ Created on Jun 1, 2012
 Provides the configurations for the resources.
 '''
 
-from ..ally_core.resources import resourcesRoot, resourcesLocator
+from ..ally_authentication_core.resources import resourcesLocatorAuthentication, \
+    resourcesRootAuthentication
 from ally.api.config import GET
 from ally.api.type import List, TypeClass
 from ally.container import ioc
@@ -18,7 +19,7 @@ from ally.core.spec.resources import Path
 
 # --------------------------------------------------------------------
 
-@ioc.after(resourcesLocator)
-def decorateRoot():
-    resourcesRoot().get = InvokerFunction(GET, resourcesLocator().findGetAllAccessible,
-                                          List(TypeClass(Path)), [], {})
+@ioc.after(resourcesLocatorAuthentication)
+def decorateRootAuthentication():
+    resourcesRootAuthentication().get = InvokerFunction(GET, resourcesLocatorAuthentication().findGetAllAccessible,
+                                                        List(TypeClass(Path)), [], {})
