02e24bd797f501f66832fa61123c283a823ad88b
Mugur Rus <mugur.rus@sourcefabric.org>
2012-11-19 17:16:39 +0200

    Removed non Ally-Py files

--- build.bat
+++ build.bat
@@ -5,8 +5,3 @@ del distribution\components\*.egg
 python.exe components\build_egg.py
 del distribution\plugins\*.egg
 python.exe plugins\build_egg.py
-del superdesk\distribution\components\*.egg
-del superdesk\distribution\plugins\*.egg
-copy distribution\plugins\*.egg superdesk\distribution\plugins\
-copy distribution\components\*.egg superdesk\distribution\components\
-python.exe superdesk\plugins\build_egg.py
