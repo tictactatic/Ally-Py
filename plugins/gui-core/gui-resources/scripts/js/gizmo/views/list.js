7664fb58cd7afe00c993000f27fc9a4a1725abd7
Mihai Balaceanu <gizm0bill@gmail.com>
2012-12-05 19:33:50 +0200

    NR-501 : Media Archive user interface implementation version 1.0
    added a suppeort for a hotfix regarding collection refresh on implementation

--- plugins/gui-core/gui-resources/scripts/js/gizmo/views/list.js
+++ plugins/gui-core/gui-resources/scripts/js/gizmo/views/list.js
@@ -45,13 +45,17 @@ function($, superdesk, giz)
             '.pagination a': { 'click': 'switchPage' }
         },
         
+        getSearchTerm: function()
+        {
+            return $('[name="search"]', self.el).val().toLowerCase();
+        },
         /*!
          * search handlers
          */
         search: function()
         {
             var self = this,
-                src = $('[name="search"]', self.el).val().toLowerCase();
+                src = this.getSearchTerm();
             if( src.length <= 1 )
             {
                 this.refresh();
@@ -241,6 +245,7 @@ function($, superdesk, giz)
          */
         renderList: function()
         {
+            if(!this.firstRender) return;
             var self = this;
             self.clearList();
             this.collection.each(function(){ self.addItem(this); });
@@ -253,6 +258,7 @@ function($, superdesk, giz)
          * the template to render
          */
         tmpl: '',
+        firstRender: false, // a sort of hack for collection
         renderCallback: $.noop,
         /*!
          * main render
@@ -263,6 +269,7 @@ function($, superdesk, giz)
             this.paginate();
             var data = {pagination: this.page},
                 self = this;
+            this.firstRender = true;
             $.tmpl(self.tmpl, data, function(e, o)
             {
                 self.el.html(o);
