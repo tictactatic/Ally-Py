702f557284b9dc8ee4cf6ba2a41df4e0cd2c4c11
ioan <ioan.pocol@sourcefabric.org>
2012-12-10 10:56:19 +0200

    Changed util service, parse quary partial

--- components/ally-core-sqlalchemy/ally/support/sqlalchemy/util_service.py
+++ components/ally-core-sqlalchemy/ally/support/sqlalchemy/util_service.py
@@ -21,6 +21,7 @@ from itertools import chain
 from sqlalchemy.exc import IntegrityError, OperationalError
 from sqlalchemy.orm.mapper import Mapper
 from sqlalchemy.orm.properties import ColumnProperty
+from sqlalchemy.sql.expression import and_, not_, or_
 
 # --------------------------------------------------------------------
 
@@ -139,5 +140,116 @@ def buildQuery(sqlQuery, query, mapped, only=None, exclude=None):
         for column, asc, __ in chain(ordered, unordered):
             if asc: sqlQuery = sqlQuery.order_by(column)
             else: sqlQuery = sqlQuery.order_by(column.desc())
+
+    return sqlQuery
+
+
+def buildPartialQuery(sqlQuery, query, mapped, queryClauses):
+    '''
+    Builds the query on the SQL alchemy query.
+    
+    @param sqlQuery: SQL alchemy
+        The sql alchemy query to use.
+    @param query: query
+        The REST query object to provide filtering on.
+    @param mapped: class
+        The mapped model class to use the query on.
+    '''
+    assert query is not None, 'A query object is required'
+    clazz = query.__class__
+
+    ordered, unordered = [], []
+    mapper = mappingFor(mapped)
+    assert isinstance(mapper, Mapper)
+
+    properties = {cp.key.lower(): getattr(mapper.c, cp.key)
+                  for cp in mapper.iterate_properties if isinstance(cp, ColumnProperty)}
+    
+    andClauses = list()
+    orClauses = list()
+
+    for criteria in namesForQuery(clazz):
+        column = properties.get(criteria.lower())
+        if column is not None and getattr(clazz, criteria) in query:
+
+            crt = getattr(query, criteria)
+            if isinstance(crt, AsBoolean):
+                assert isinstance(crt, AsBoolean)
+                if AsBoolean.value in crt:
+                    if crt.operator == 'or': orClauses.append(column == crt.value)
+                    elif crt.operator == 'not': andClauses.append(not_(column == crt.value))
+                    else: andClauses.append(column == crt.value)
+            elif isinstance(crt, AsLike):
+                assert isinstance(crt, AsLike)
+                if AsLike.like in crt: 
+                    if crt.operator == 'or': orClauses.append(column.like(crt.like))
+                    elif crt.operator == 'not': andClauses.append(not_(column.like(crt.like)))
+                    else: andClauses.append(column.like(crt.like))
+                elif AsLike.ilike in crt: 
+                    if crt.operator == 'or': orClauses.append(column.ilike(crt.ilike))
+                    elif crt.operator == 'not': andClauses.append(not_(column.ilike(crt.ilike)))
+                    else: andClauses.append(column.ilike(crt.ilike))
+            elif isinstance(crt, AsEqual):
+                assert isinstance(crt, AsEqual)
+                if AsEqual.equal in crt:
+                    if crt.operator == 'or': orClauses.append(column == crt.equal)
+                    elif crt.operator == 'not': andClauses.append(not_(column == crt.equal))
+                    else: andClauses.append(column.ilike(column == crt.equal))
+            elif isinstance(crt, (AsDate, AsTime, AsDateTime, AsRange)):
+                if crt.__class__.start in crt: 
+                    if crt.operator == 'or': orClauses.append(column >= crt.start)
+                    elif crt.operator == 'not': andClauses.append(not_(column >= crt.start))
+                    else: andClauses.append(column.ilike(column >= crt.start))
+                elif crt.__class__.until in crt: 
+                    if crt.operator == 'or': orClauses.append(column < crt.until)
+                    elif crt.operator == 'not': andClauses.append(not_(column < crt.until))
+                    else: andClauses.append(column.ilike(column < crt.until))
+                if crt.__class__.end in crt: 
+                    if crt.operator == 'or': orClauses.append(column <= crt.end)
+                    elif crt.operator == 'not': andClauses.append(not_(column <= crt.end))
+                    else: andClauses.append(column.ilike(column <= crt.end))
+                elif crt.__class__.since in crt: 
+                    if crt.operator == 'or': orClauses.append(column > crt.since)
+                    elif crt.operator == 'not': andClauses.append(not_(column > crt.since))
+                    else: andClauses.append(column.ilike(column > crt.since))
+
+            if isinstance(crt, AsOrdered):
+                assert isinstance(crt, AsOrdered)
+                if AsOrdered.ascending in crt:
+                    if AsOrdered.priority in crt and crt.priority:
+                        ordered.append((column, crt.ascending, crt.priority))
+                    else:
+                        unordered.append((column, crt.ascending, None))
+
+            ordered.sort(key=lambda pack: pack[2])
+            for column, asc, __ in chain(ordered, unordered):
+                if asc: sqlQuery = sqlQuery.order_by(column)
+                else: sqlQuery = sqlQuery.order_by(column.desc())               
+
+    lengthAnd = len(andClauses)
+    lengthOr = len(orClauses)
+    
+    if lengthAnd == 0:
+        if lengthOr == 0:
+            #do nothing
+            pass
+        elif lengthOr == 1:   
+            queryClauses.append(orClauses[0])
+        elif lengthOr > 1:
+            queryClauses.append(or_(*orClauses))
+    elif lengthAnd == 1:
+        if lengthOr == 0:
+            queryClauses.append(andClauses[0])
+        elif lengthOr == 1:   
+            queryClauses.append(or_(andClauses[0], orClauses[0]))
+        elif lengthOr > 1:
+            queryClauses.append(or_(andClauses[0], or_(*orClauses)))
+    elif lengthAnd > 1:
+        if lengthOr == 0:
+            queryClauses.append(and_(*andClauses)) 
+        elif lengthOr == 1:   
+            queryClauses.append(or_(and_(*andClauses), orClauses[0]))
+        elif lengthOr > 1:
+            queryClauses.append(or_(and_(*andClauses), or_(*orClauses)))
 
     return sqlQuery
