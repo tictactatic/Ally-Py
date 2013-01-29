1cf8b5302b7d539839f0c4002efa1aeea47d2041
Gabriel Nistor <gabriel.nistor@sourcefabric.org>
2013-01-29 13:19:15 +0200

    Quick fix for the actions count.

--- plugins/gui-action/gui/action/impl/action.py
+++ plugins/gui-action/gui/action/impl/action.py
@@ -1,18 +1,19 @@
 '''
 Created on Feb 27, 2012
 
-@package ally 
-@copyright 2011 Sourcefabric o.p.s.
-@license http://www.gnu.org/licenses/gpl-3.0.txt
+@package: gui action
+@copyright: 2011 Sourcefabric o.p.s.
+@license: http://www.gnu.org/licenses/gpl-3.0.txt
 @author: Mihai Balaceanu
 
 Action Manager Implementation
 '''
 
-import re
 from ally.container.ioc import injected
 from ally.container.support import setup
+from ally.support.api.util_service import copy
 from gui.action.api.action import IActionManagerService, Action
+import re
 
 # --------------------------------------------------------------------
 
@@ -46,10 +47,29 @@ class ActionManagerService(IActionManagerService):
                 actions = [action for action in actions if action.Path == path.strip('"')]
             # match a word placeholder *
             elif path.find('*') != -1:
-                p = '^'+re.sub(r'\\\*', '(\d|\w|-|_)+', re.escape(path))+'$'
+                p = '^' + re.sub(r'\\\*', '(\d|\w|-|_)+', re.escape(path)) + '$'
                 actions = [action for action in actions if re.match(p, action.Path)]
             # normal match, paths starting with path string
             else: 
                 actions = [action for action in actions if action.Path.startswith(path.rstrip('.'))]
                 
-        return sorted(actions, key=lambda action: action.Path)
+        return processChildCount(actions)
+
+# --------------------------------------------------------------------
+
+# TODO: this will get removed after the super x-filter implementation.
+def processChildCount(actions):
+    '''
+    Process the child count for the provided actions list.
+    '''
+    actions = sorted(actions, key=lambda action: action.Path)
+    for k, action in enumerate(actions):
+        actionWithCount = Action()
+        copy(action, actionWithCount, exclude=('ChildrenCount',))
+        childCount, path = 0, action.Path + '.'
+        for i in range(k + 1, len(actions)):
+            if actions[i].Path.startswith(path): childCount += 1
+            else: break
+        actionWithCount.ChildrenCount = childCount
+        yield actionWithCount
+        
