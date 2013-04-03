d6bad717cc245a09faeafc398aa3bfa802cf8132
petr.jasek <petr.jasek@sourcefabric.org>
2013-04-03 10:37:15 +0200

    Set backbone jquery if not set.

--- plugins/gui-core/gui-resources/scripts/js/main.js
+++ plugins/gui-core/gui-resources/scripts/js/main.js
@@ -51,6 +51,10 @@ require(['concat', 'backbone'], function(){
 	], 
 	function(MenuView, authView, $, superdesk, Action, router)
 	{
+        if (Backbone.$ === undefined) {
+            Backbone.$ = $;
+        }
+
 	    $(authView).on('logout login', function(){ Action.clearCache(); });
 
         // initialize menu before auth because we have some events bound to auth there
