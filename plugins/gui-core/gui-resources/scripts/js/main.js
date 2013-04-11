f5b8a62746b9362b42c3c9b43b2778e2a6d162b9
petr.jasek <petr.jasek@sourcefabric.org>
2013-04-11 11:37:37 +0200

    Fix backbone/underscore loading.
    Underscore is required for backbone but _ is overriden
    via localization. Backbone is now loaded in top require
    to make sure it gets underscore.

--- plugins/gui-core/gui-resources/scripts/js/main.js
+++ plugins/gui-core/gui-resources/scripts/js/main.js
@@ -50,10 +50,6 @@ require(['concat', 'backbone'], function(){
 	], 
 	function(MenuView, authView, $, superdesk, Action, Backbone)
 	{
-        if (Backbone.$ === undefined) {
-            Backbone.$ = $;
-        }
-
 	    $(authView).on('logout login', function(){ Action.clearCache(); });
 
         // initialize menu before auth because we have some events bound to auth there
