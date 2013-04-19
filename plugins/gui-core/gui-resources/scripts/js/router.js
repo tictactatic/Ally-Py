936d46ee94bab68745aa4d164a74ce06235d7626
petr.jasek <petr.jasek@sourcefabric.org>
2013-04-19 12:20:12 +0200

    Add start method to router.
    For submodules having their own router define start method
    which can be called many times and triggers loadUrl if there
    was no route matching before.

--- plugins/gui-core/gui-resources/scripts/js/router.js
+++ plugins/gui-core/gui-resources/scripts/js/router.js
@@ -1,16 +1,21 @@
 define(['backbone'], function(Backbone) {
-
-
     var Router = {
         hasRoute: Backbone.history.start({root: config.lib_url + 'start.html'}),
 
         /**
-         * Add route to global router and try to active it
-         * if there was no route matching current fragment.
+         * Add route to global router and start
          */
         route: function(route, name, callback) {
             var router = new Backbone.Router();
             router.route(route, name, callback);
+            this.start();
+        },
+
+        /**
+         * Try to find a route matching current fragment
+         * if non there was non before
+         */
+        start: function() {
             if (!this.hasRoute) {
                 this.hasRoute = Backbone.history.loadUrl();
             }
