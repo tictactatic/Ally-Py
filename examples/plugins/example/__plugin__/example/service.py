f4046fff603de52d4dfacefeca8bcae14696c31f
Martin Saturka <martin.saturka@sourcefabric.org>
2013-06-13 12:55:59 +0200

    APY-31: Update the code from Ally-Py tutorial * some comments in

--- examples/plugins/example/__plugin__/example/service.py
+++ examples/plugins/example/__plugin__/example/service.py
@@ -17,14 +17,22 @@ from itertools import chain
 # --------------------------------------------------------------------
 
 SERVICES = 'example.*.api.**.I*Service'
+
+# this provides the (start/rollback/commit) session processing
 @ioc.entity
 def binders(): return [bindExampleSession]
+
+# this provides validation of input data against database structure and content
 @ioc.entity
 def bindersService(): return list(chain((bindExampleValidations,), binders()))
 
+# inner binding; for implementation services together with alchemy database classes
 bind.bindToEntities('example.*.impl.**.*Alchemy', binders=binders)
+# inner binding; for implementation services
 support.createEntitySetup('example.*.impl.**.*')
+# outer binding; for API registering
 support.listenToEntities(SERVICES, listeners=addService(bindersService))
+# outer binding; assuring that all APIs are registered
 support.loadAllEntities(SERVICES)
 
 # --------------------------------------------------------------------
