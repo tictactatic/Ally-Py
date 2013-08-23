f079a0941c16837e4c4917320b72093f812e56e7
Nikola Nikolic <nikolic_nikola@live.com>
2013-08-23 12:17:40 +0200

    LB-839 : Add 'Loading' wheel whenever asynchronous info is loading
    Changes from param to methods

--- plugins/gui-core/gui-resources/scripts/js/jquery/loadingblock.js
+++ plugins/gui-core/gui-resources/scripts/js/jquery/loadingblock.js
@@ -1,48 +1,46 @@
 /*
     Use:
-        - $('#element').loadingblock();                 to make loading block over an element, without any msg
-        - $('#element').loadingblock('Some msg here');  to make a loading block over an element, with text msg
+        - $('#element').loadingblock();        //to initialize loading block over an element
 
-        - $('#element').loadingblock('hide');           to hide loading block once it is initialize
-        - $('#element').loadingblock('show');           to show loading block once it is initialize
+        - $('#element').loadingblock({         //function additional params        
+            message   : 'This is message',     //add text message to loading block
+            toggle    : true/false             //show/hide loading block once it is initialized
+            destroy   : true/false             //destroy loading block when it is no more needed, false is default
+            changePos : true/false             //change postitioning style of parent block , true is default         
+          });  
 
-        - $('#element').loadingblock('destroy');        to destroy oading block
-        
-    Calling .loadingblock() on already the element that already contained one, will result with showing new
-    text and making it visible again. (so overriding the old one)
 
-    It is not possible to use loadign block on elements that are not able to have child nodes (like <input>)
+    Calling .loadingblock() on the element that already contained one, will result with overriding the old one
+
+    It is not possible to use loading block on elements that are not able to have child nodes (like <input>)
 */
 
 (function ($) {
  
-    $.fn.loadingblock = function(text) {
+    $.fn.loadingblock = function(options) {
+        
+        var s = $.extend({
+            toggle  : true,
+            message : '',
+            destroy : false,
+            changePos : true
+        }, options );
+
+
         var _block = this.find("> .async-loading");
         var _exist = _block.length > 0 ? true : false;
-        switch (text) {
-            case 'hide' : 
-                        _exist == true && _block.hide();
-                        break;
-            case 'show' :  
-                        _exist == true && _block.show();
-                        break;
-            case 'destroy' : 
-                        _exist == true && _block.remove();
-                        break;
-            default : 
-                        var msg = ( text === undefined ? '' : text);
-                        if ( _exist == true) {
-                            _block.find(".loading-msg").html(msg);
-                            _block.show();
-                        }
-                        else {
-                            var _pos = this.css('position');
-                            (_pos !== 'absolute' && _pos !== 'relative' && _pos !== 'fixed') && this.css('position','relative');
-                            var template = '<div class="async-loading"><div class="loading-msg">' + msg + '</div></div>';
-                            this.append(template);
-                        }
-                        break;
+        var _msg = _block.find(".loading-msg")
+
+        if (_exist) {
+            s.destroy ? (_block.remove()) : (s.toggle ? (_msg.html(s.message), _block.show()) : (_block.hide(),_msg.html(s.message)));
         }
+        else {
+            var _pos = this.css('position');
+            (_pos !== 'absolute' && _pos !== 'relative' && _pos !== 'fixed' && s.changePos) && this.css('position','relative');
+            var template = '<div class="async-loading"><div class="loading-msg">' + s.message + '</div></div>';
+            this.append(template);
+        }
+
         return this;
     }; 
 }( jQuery ));
